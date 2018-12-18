59198d9fbcf0b1302ff1b1c804e88ad83f02c65a
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-12-18 17:39:44 +0100

    Init Han

---
+++ Script.R
@@ -0,0 +1,13 @@
+pannes = read.csv(
+    file = "FailureTimes_5.csv",
+    header = TRUE,
+    sep = ",",
+    dec = ".",
+    colClasses = c("NULL", NA)
+);
+hist(
+     pannes$Heures,
+     breaks = 20
+);
+
+library(stats4);
