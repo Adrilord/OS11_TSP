2f9ba5344e6be19850bece3c0bf07912536fff6d
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2019-01-07 16:42:58 +0100

    Clean up

--- density_distance.R
+++
@@ -1,7 +0,0 @@
-density_distance = function(
-    F_X, # CDF with one parameter
-    H # histogramme of data
-) {
-    n = length(H$breaks)
-    sum(abs((F_X(H$breaks[2:n]) - F_X(H$breaks[1:n - 1])) - H$density))
-}
